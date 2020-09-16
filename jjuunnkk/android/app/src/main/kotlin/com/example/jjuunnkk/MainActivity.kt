026b8a3549c3c8e17e268b02b31fdad249e80286
Gopal Goyal <51030583+anonymouslyknown@users.noreply.github.com>
2020-09-16 18:11:01 +0530

    added first two projects
    flutter projectss

---
+++ jjuunnkk/android/app/src/main/kotlin/com/example/jjuunnkk/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.jjuunnkk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ testing_app/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+// the main function is the starting point of all flutter apps
+  void main() {
+    runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            backgroundColor: Colors.blueGrey[900],
+            title: Text('This is total JjuuunnnkK'),
+          ),
+          body: Center(
+              child: Image(
+                image: AssetImage('assets/image/jjunkkselfie.jpg'),
+              )
+          ),
+        ),
+      ),
+    );
+}
