77e7c75d343b67a5028def49d44e86f77e2d911d
Gopal Goyal <51030583+anonymouslyknown@users.noreply.github.com>
2020-09-29 13:10:09 +0530

    Working on climate app

--- Clima-Flutter/lib/screens/loading_screen.dart
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,24 +8,31 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getLocation() async {
-    Position position =
-        await getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-    print(position);
+  @override
+  void initState() {
+    getLocation();
+    super.initState();
   }
+
+  void getLocation() async{
+    Location location = Location();
+
+    await location.getCurrentLocation();
+
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  void getData() async{
+    http.Response response = await http.get('api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=2bsg5334nsios83nw933h38');
+    print(response.statusCode);
+    print(response.body);
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            requestPermission();
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    getData();
+    return Scaffold();
   }
 }
